86ca4a4d602a99ad32bd282b76dc02f2471e0924
Kevin Westropp <mail@kevinpatrickwestropp.com>
2023-01-18 14:04:17 -0700

    Changes: - Update nvm/node to use 16 - Update functions - Update github workflows

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,27 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as cors from 'cors';
+import {Response} from 'express';
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://highlanderkev-github-io.firebaseio.com',
+});
+
+const corsOptions = {
+  origin: true,
+};
+
+const corsHandler = cors(corsOptions);
+
+function errorHandler(res: Response) {
+  return (error: any): void => {
+    console.error(error);
+    res.send(500);
+  };
+}
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info('Hello logs!', {structuredData: true});
+  response.send(`Hello from Firebase! Config value: ${process.env.TEST}\n\n`);
+});
